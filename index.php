e8cb133d855a0fc4dc7b84038f36c678f6580bf3
d-pamneja <dpamneja@gmail.com>
2024-07-27 16:57:12 +0530

    active

--- index.php
+++ index.php
@@ -119,19 +119,8 @@ Author URL: http://w3layouts.com
     </script>
         <!-- Add active dynamically-->
 
-    <script>
-document.addEventListener("DOMContentLoaded", function() {
-    var path = window.location.pathname;
-    var page = path.split("/").pop(); // Get the file name from the URL
 
-    var navLinks = document.querySelectorAll(".nav-link");
-    navLinks.forEach(function(link) {
-        if (link.getAttribute("href") === page) {
-            link.classList.add("active");
-        }
-    });
-});
-</script>
+    <script src="assets/js/active.js"></script>
 
     <!-- //disable body scroll which navbar is in active -->
     <!-- //bootstrap -->
