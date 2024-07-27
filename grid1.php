93fe569b1daa6a7477e426d97da9050a23123917
d-pamneja <dpamneja@gmail.com>
2024-07-27 16:06:51 +0530

    page break

---
+++ grid1.php
@@ -0,0 +1,18 @@
+<section class="w3l-grids-3 py-5" id="about">
+    <div class="container py-md-5 py-3">
+        <div class="row w3home-ab-grids">
+            <div class="w3ab-right-top col-lg-6 pe-lg-5">
+                <img src="assets/images/g2.jpg" alt="" class="img-fluid radius-image">
+            </div>
+            <div class="w3ab-left-top col-lg-6 mt-lg-0 mt-4 pt-lg-0 pt-2">
+                <h6 class="title-subw3hny mb-1">Get rather healthy.</h6>
+                <h3 class="title-w3l mb-2">Welcome to Our Poultry Field.</h3>
+                <p class="my-3"> Lorem ipsum viverra feugiat. Pellen tesque libero ut justo, ultrices in ligula. Semper at tempufddfel.Lorem ipsum dolor sit, amet consectetur ante ipsum elit. faucibus orci luctus et ultrices posuere cubilia Curae;
+                    Nulla mollis dapibus nunc. </p>
+
+                <a href="about.html" class="btn btn-style btn-primary mt-4">Read More</a>
+            </div>
+
+        </div>
+    </div>
+</section>
---
+++ main_slider.php
@@ -0,0 +1,29 @@
+<section class="w3l-main-slider banner-slider" id="home">
+    <div class="owl-one owl-carousel owl-theme">
+            <div class="slider-info banner-view banner-top1">
+                <div class="container">
+                    <div class="w3banner-grids">
+                        <div class="banner-info header-hero-19">
+                            <p class="w3hny-tag">Loyal to quality</p>
+                            <h3 class="title-hero-19">The chicken always comes chief.</h3>
+                            <a href="about.html" class="btn btn-style btn-primary mt-4">Read More </a>
+                        </div>
+                        <div class="banner-left position-relative mt-5">
+                            <a href="#small-dialog" class="popup-with-zoom-anim play-view text-center position-absolute">
+                                <span class="video-play-icon">
+                                    <span class="fa fa-play"></span>
+                                </span>
+                            </a>
+                            <!-- dialog itself, mfp-hide class is required to make dialog hidden --
+                            <div id="small-dialog" class="zoom-anim-dialog mfp-hide">
+                                <iframe src="https://player.vimeo.com/video/26489864?h=0cb4432fb5" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+                                
+                            </div>
+                            <!-- dialog itself, mfp-hide class is required to make dialog hidden -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+
+</section>
