3a3d22ed4816092e4cf836efc06aaac4f3a8a342
d-pamneja <dpamneja@gmail.com>
2024-07-29 16:51:14 +0530

    Agro - Content

--- agro.php
+++ agro.php
@@ -43,35 +43,64 @@
     <!--/w3l-index3-->
     <section class="w3l-index-about py-5" id="about2">
         <div class="container py-md-5 py-2">
-            <div class="row">
-                <div class="col-lg-6 pe-lg-5">
+            <div class="row align-items-center">
+                <div class="col-lg-6 order-lg-2">
                     <div class="w3l-abouthny-img">
-                        <img src="assets/images/ab4.jpg" alt="" class="img-fluid radius-image">
+                        <img src="assets/images/agro_banner1.jpeg" alt="" class="img-fluid radius-image">
                     </div>
                 </div>
-                <div class="col-lg-6 mt-lg-0 mt-5">
+
+                <div class="col-lg-6 order-lg-1 text-center">
                     <div class="w3l-abouthny-info">
                         <h6 class="title-subw3hny">What We Do</h6>
-                        <h3 class="title-w3l">We give you complete control
-                            of your Poultry Fields.</h3>
-                        <p class="mt-4">Lorem ipsum viverra feugiat. Pellen tesque libero ut justo, ultrices in ligula.
-                            Semper at tempufddfel.Lorem ipsum dolor sit, amet consectetur elit. Earum mollitia cum ex
-                            ipsam autem!earum sequi amet.</p>
+                        <h3 class="title-w3l">Nurturing Australian Agriculture, Cultivating a Sustainable Future</h3>
+                        <p class="mt-4">
+                            Our Agrochemical Division in Australia is deeply rooted in scientific innovation and
+                            collaborative partnerships. We're passionate about empowering Australian farmers and the
+                            agricultural industry as a whole through cutting-edge agrochemical solutions that boost
+                            productivity while minimizing environmental impact.
+                        </p>
                         <ul class="w3l-right-book w3l-right-book-grid mt-md-5 mt-4">
-                            <li><span class="fas fa-check"></span> Fresh Chiken</li>
-                            <li><span class="fas fa-check"></span> Raw Chiken Broiler</li>
-                            <li><span class="fas fa-check"></span> Brown Eggs</li>
-                            <li><span class="fas fa-check"></span> White Eggs</li>
-                            <li><span class="fas fa-check"></span> 24/7 Hours Support</li>
-                            <li><span class="fas fa-check"></span> Fast Delivery</li>
+                            <li><span class="fas fa-check"></span> Crop Protection Solutions</li>
+                            <li><span class="fas fa-check"></span> Tailored Agronomic Advice</li>
+                            <li><span class="fas fa-check"></span> Sustainable Farming Practices</li>
+                            <li><span class="fas fa-check"></span> Research & Development Partnerships</li>
+                            <li><span class="fas fa-check"></span> Regulatory Compliance Support</li>
+                            <li><span class="fas fa-check"></span> 24/7 Technical Assistance</li>
                         </ul>
-                        <a href="#services" class="btn btn-style btn-primary mt-4">Read More</a>
                     </div>
                 </div>
             </div>
         </div>
     </section>
     <!--//w3l-index3-->
+
+    <section class="hero-section">
+        <div class="hero-content container py-md-5 py-2">
+            <div class="row align-items-center">
+                <div class="col-lg-6 order-lg-1 text-center"> <div class="w3l-abouthny-img text-center">
+                        <img src="assets/images/agro_image4.jpeg" alt="" class="img-fluid radius-image">
+                    </div>
+                </div>
+                <div class="col-lg-6 order-lg-2 text-center"> <div class="w3l-abouthny-info">
+                        <h3 class="title-w3l">Trusted Partnership in Agrochemical Solutions</h3>
+                        <p class="mt-4">
+                            Vinayak enterprises is proud to be a fully APVMA-registered provider of agrochemical solutions, adhering to the highest standards of safety and efficacy. Our commitment to excellence is reflected in our comprehensive certifications and meticulous adherence to Material Safety Data Sheets (MSDS) protocols.
+                        </p>
+                        <p class="mt-4">
+                            We understand the importance of responsible agrochemical use for both the environment and the well-being of Australian farmers. That's why we invest in rigorous research and development, ensuring our products not only deliver exceptional results but also minimise environmental impact.
+                        </p>
+                        <p class="mt-4">
+                            When you partner with us, you're choosing a company that values transparency, professionalism, and a deep commitment to the sustainability of Australian agriculture.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
     <!--/features-section-->
     <section class="w3l-features w3l-services-page py-5" id="work">
         <div class="container py-lg-5 py-md-4 py-2">
@@ -84,69 +113,66 @@
                     <div class="col-lg-4 col-md-6 mt-lg-5 mt-4">
                         <div class="grids-1 box-wrap">
                             <div class="icon">
-                                <i class="fas fa-egg"></i>
+                                <i class="fas fa-leaf"></i>
                             </div>
                             <div class="icon-info text-left">
-                                <h4><a href="#service" class="title-head mt-0">Alternative egg</a></h4>
-                                <p class="text-para">Lorem ipsum dolor sit amet, elit. Id ab commodi magnam. </p>
+                                <h4 class="title-head mt-0">Crop Protection</h4>
+                                <p class="text-para">Comprehensive pest and disease management solutions, including insecticides, fungicides, and herbicides, tailored to specific crop needs. </p>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-6 mt-lg-5 mt-4">
                         <div class="grids-1 box-wrap">
                             <div class="icon">
-                                <i class="fas fa-warehouse"></i>
+                                <i class="fas fa-tree"></i>
                             </div>
                             <div class="icon-info text-left">
-                                <h4><a href="#service" class="title-head mt-0">
-                                        Poultry Cages</a></h4>
-                                <p class="text-para">Lorem ipsum dolor sit amet, elit. Id ab commodi magnam. </p>
+                                <h4 class="title-head mt-0"> Soil Health & Fertility</a></h4>
+                                <p class="text-para">Soil analysis, nutrient management plans, and fertilizer recommendations to optimize soil health and fertility for increased yields. </p>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-6 mt-lg-5 mt-4">
                         <div class="grids-1 box-wrap">
                             <div class="icon">
-                                <i class="fas fa-kiwi-bird"></i>
+                                <i class="fas fa-seedling"></i>
                             </div>
                             <div class="icon-info text-left">
-                                <h4><a href="#service" class="title-head mt-0">Breeder Management</a></h4>
-                                <p class="text-para">Lorem ipsum dolor sit amet, elit. Id ab commodi magnam. </p>
+                                <h4 class="title-head mt-0">Seed Treatment</h4>
+                                <p class="text-para">Seed treatment services to protect seeds from pests and diseases, ensuring healthy germination and vigorous growth. </p>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-6 mt-lg-5 mt-4">
                         <div class="grids-1 box-wrap">
                             <div class="icon">
-                                <i class="fas fa-feather"></i>
+                                <i class="fas fa-tint"></i>
                             </div>
                             <div class="icon-info text-left">
-                                <h4><a href="#service" class="title-head mt-0">Poultry Climate</a></h4>
-                                <p class="text-para">Lorem ipsum dolor sit amet, elit. Id ab commodi magnam. </p>
+                                <h4 class="title-head mt-0">Sustainable Agriculture Solutions</h4>
+                                <p class="text-para">Consulting on sustainable farming practices to minimize environmental impact while maximizing productivity for farmers.</p>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-6 mt-lg-5 mt-4">
                         <div class="grids-1 box-wrap">
                             <div class="icon">
-                                <i class="fas fa-sign-language"></i>
+                                <i class="fas fa-handshake"></i>
                             </div>
                             <div class="icon-info text-left">
-                                <h4><a href="#service" class="title-head mt-0">
-                                        Residue Teatment</a></h4>
-                                <p class="text-para">Lorem ipsum dolor sit amet, elit. Id ab commodi magnam. </p>
+                                <h4 class="title-head mt-0">Regulatory Compliance</h4>
+                                <p class="text-para">Guidance on regulatory compliance for agrochemical use, ensuring adherence to safety standards and environmental regulations. </p>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-6 mt-lg-5 mt-4">
                         <div class="grids-1 box-wrap">
                             <div class="icon">
-                                <i class="fas fa-fan"></i>
+                                <i class="fas fa-search"></i>
                             </div>
                             <div class="icon-info text-left">
-                                <h4><a href="#service" class="title-head mt-0">
-                                        Exhaust air Treatment</a></h4>
-                                <p class="text-para">Lorem ipsum dolor sit amet, elit. Id ab commodi magnam. </p>
+                                <h4 class="title-head mt-0">Personalized Agronomic Advice</h4>
+                                <p class="text-para">Tailored advice from experienced agronomists on crop management practices, including planting, fertilization, and harvest timing.</p>
                             </div>
                         </div>
                     </div>
@@ -155,44 +181,60 @@
         </div>
     </section>
     <!--//features-section -->
-
     <!--/w3l-grids-->
     <section class="w3l-witemshny-main py-5">
+        <div class="title-content text-center mb-lg-3 mb-4">
+            <h6 class="title-subw3hny mb-1">Our Mission</h6>
+            <h3 class="title-w3l">How We Deliever</h3>
+        </div>
         <div class="container py-md-4">
             <!--/row-1-->
             <div class="witemshny-grids row">
                 <div class="col-md-4 col-6 product-incfhny mt-4">
                     <div class="weitemshny-grid oposition-relative">
-                        <a href="#img" class="d-block zoom"><img src="assets/images/g1.jpg" alt=""
+                        <a href="#img" class="d-block zoom"><img src="assets/images/agro_image1.jpeg" alt=""
                                 class="img-fluid news-image"></a>
                         <div class="witemshny-inf">
                         </div>
                     </div>
-                    <h4 class="gdnhy-1 mt-lg-4 mt-3"><a href="#img">We are one of leading company</a>
+                    <h4 class="gdnhy-1 mt-lg-4 mt-3"><a href="#img">Understanding the Land, Empowering the Farmer</a>
                     </h4>
-                    <p class="mt-md-2">Lorem ipsum dolor sit amet, elit. Id ab commodi magnam.</p>
+                    <p class="mt-md-2">
+                        Our deep respect for Australian farmers and their connection to the land drives everything we
+                        do.
+                        We collaborate closely with farmers to understand their unique challenges and develop tailored
+                        agrochemical solutions that maximize yields and protect precious resources.
+                    </p>
                 </div>
                 <div class="col-md-4 col-6 product-incfhny mt-4">
                     <div class="weitemshny-grid oposition-relative">
-                        <a href="#img" class="d-block zoom"><img src="assets/images/g6.jpg" alt=""
+                        <a href="#img" class="d-block zoom"><img src="assets/images/agro_image2.jpeg" alt=""
                                 class="img-fluid news-image"></a>
                         <div class="witemshny-inf">
                         </div>
                     </div>
-                    <h4 class="gdnhy-1 mt-lg-4 mt-3"><a href="#img"> The primary goal of all business</a>
+                    <h4 class="gdnhy-1 mt-lg-4 mt-3"><a href="#img">Science Meets Soil</a>
                     </h4>
-                    <p class="mt-md-2">Lorem ipsum dolor sit amet, elit. Id ab commodi magnam.</p>
+                    <p class="mt-md-2">
+                        We're committed to continuous research and development, working hand-in-hand with leading
+                        scientists and agronomists to pioneer innovative agrochemicals that are safe, effective, and
+                        environmentally responsible.
+                    </p>
                 </div>
                 <div class="col-md-4 col-6 product-incfhny mt-4">
                     <div class="weitemshny-grid oposition-relative">
-                        <a href="#img" class="d-block zoom"><img src="assets/images/g2.jpg" alt=""
+                        <a href="#img" class="d-block zoom"><img src="assets/images/agro_image3.jpeg" alt=""
                                 class="img-fluid news-image"></a>
                         <div class="witemshny-inf">
                         </div>
                     </div>
-                    <h4 class="gdnhy-1 mt-lg-4 mt-3"><a href="#img"> Learn how to grow Business</a>
+                    <h4 class="gdnhy-1 mt-lg-4 mt-3"><a href="#img">Technology for a Sustainable Future</a>
                     </h4>
-                    <p class="mt-md-2">Lorem ipsum dolor sit amet, elit. Id ab commodi magnam.</p>
+                    <p class="mt-md-2">
+                        We leverage cutting-edge technologies like precision agriculture and data analytics to empower
+                        farmers with the tools they need to make informed decisions, optimize their operations, and
+                        build a more sustainable future for Australian agriculture.
+                    </p>
                 </div>
             </div>
             <!--//row-1-->
