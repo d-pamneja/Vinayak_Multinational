07122be459ffb88e507eba1220f813598cfa7eb0
d-pamneja <dpamneja@gmail.com>
2024-08-02 14:37:27 +0530

    final content corrections for #issues - 43

--- main_slider.php
+++ main_slider.php
@@ -5,9 +5,30 @@
                 <div class="w3banner-grids">
                     <div class="banner-info header-hero-19">
                         <p class="w3hny-tag">Decades of Experience, Cutting-Edge Expertise</p>
-                        <h3 class="title-hero-19">Global Expertise, Trusted Solutions: Your Advantage in the Life
-                            Sciences.</h3>
-                        <a href="about.php" class="btn btn-style btn-primary mt-4">Read More </a>
+                        <h3 class="title-hero-19">Global Expertise, Trusted Solutions: Your Advantage in the Life Sciences.</h3>
+                        <a href="about.php" class="btn btn-style btn-primary mt-4">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="slider-info banner-view banner-top1">
+            <div class="container">
+                <div class="w3banner-grids">
+                    <div class="banner-info header-hero-19">
+                        <p class="w3hny-tag">Decades of Experience, Cutting-Edge Expertise</p>
+                        <h3 class="title-hero-19">Global Expertise, Trusted Solutions: Your Advantage in the Life Sciences.</h3>
+                        <a href="about.php" class="btn btn-style btn-primary mt-4">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="slider-info banner-view banner-top1">
+            <div class="container">
+                <div class="w3banner-grids">
+                    <div class="banner-info header-hero-19">
+                        <p class="w3hny-tag">Decades of Experience, Cutting-Edge Expertise</p>
+                        <h3 class="title-hero-19">Global Expertise, Trusted Solutions: Your Advantage in the Life Sciences.</h3>
+                        <a href="about.php" class="btn btn-style btn-primary mt-4">Read More</a>
                     </div>
                 </div>
             </div>
