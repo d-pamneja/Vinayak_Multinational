a2a99628a82fd060fcc6c16247847d994ce949e1
d-pamneja <dpamneja@gmail.com>
2024-08-05 12:06:55 +0530

    final content corrections for #issues - 74

--- main_slider.php
+++ main_slider.php
@@ -1,34 +1,34 @@
 <section class="w3l-main-slider banner-slider" id="home">
     <div class="owl-one owl-carousel owl-theme">
-        <div class="slider-info banner-view banner-top1">
+        <div class="slider-info banner-view1 banner-top1">
             <div class="container">
                 <div class="w3banner-grids">
                     <div class="banner-info header-hero-19">
-                        <p class="w3hny-tag">Decades of Experience, Cutting-Edge Expertise</p>
-                        <h3 class="title-hero-19">Global Expertise, Trusted Solutions: Your Advantage in the Life Sciences.</h3>
-                        <a href="about.php" class="btn btn-style btn-primary mt-4">Read More</a>
+                        <p class="w3hny-tag">From Seed to Success, We Nurture Growth</p>
+                        <h3 class="title-hero-19">Rooted in Experience, Cultivating a Bountiful Future for Agriculture.</h3>
+                        <a href="agro.php" class="btn btn-style btn-primary mt-4">Read More</a>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="slider-info banner-view banner-top1">
+        <div class="slider-info banner-view2 banner-top2">
             <div class="container">
                 <div class="w3banner-grids">
                     <div class="banner-info header-hero-19">
                         <p class="w3hny-tag">Decades of Experience, Cutting-Edge Expertise</p>
                         <h3 class="title-hero-19">Global Expertise, Trusted Solutions: Your Advantage in the Life Sciences.</h3>
-                        <a href="about.php" class="btn btn-style btn-primary mt-4">Read More</a>
+                        <a href="pharma.php" class="btn btn-style btn-primary mt-4">Read More</a>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="slider-info banner-view banner-top1">
+        <div class="slider-info banner-view3 banner-top3">
             <div class="container">
                 <div class="w3banner-grids">
                     <div class="banner-info header-hero-19">
-                        <p class="w3hny-tag">Decades of Experience, Cutting-Edge Expertise</p>
-                        <h3 class="title-hero-19">Global Expertise, Trusted Solutions: Your Advantage in the Life Sciences.</h3>
-                        <a href="about.php" class="btn btn-style btn-primary mt-4">Read More</a>
+                        <p class="w3hny-tag">Your Vision, Our Expertise, A Successful Partnership</p>
+                        <h3 class="title-hero-19">Let's Build a Brighter Future for Your Business Together.</h3>
+                        <a href="consulting.php" class="btn btn-style btn-primary mt-4">Read More</a>
                     </div>
                 </div>
             </div>
